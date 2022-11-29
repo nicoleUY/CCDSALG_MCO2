289f752286848541569613a1a7b43d01c3614069
Smile-369 <105698549+Smile-369@users.noreply.github.com>
2022-11-29 14:25:51 +0800

    Initial commit

---
+++ src/Substring.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Substring {
+
+    static String[] kmerArrayCreation(String Input,int k){
+        ArrayList<String> temp=new ArrayList<>();
+        int i = 0;
+        while(i+k<Input.length()){
+            temp.add(Input.substring(i,i+k));
+            i++;
+        }
+        String[] output= new String[temp.size()];
+        output=temp.toArray(output);
+        return output;
+    }
+    public static String createRandomString(int n){
+        String Input ="acgt";
+        StringBuilder s = new StringBuilder(n);
+        for ( int i=0; i<n; i++) {
+            int ch = (int)(Input.length() * Math.random());
+            s.append(Input.charAt(ch));
+        }
+
+        return s.toString();
+
+    }
+
+}
